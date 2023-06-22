848391c52e9cb0dc37831500e24c7d293f84fd14
HoeksMJ <hoeksmj@gmail.com>
2023-06-22 14:03:28 -0500

    add rough capability to fetch api function

---
+++ script.js
@@ -0,0 +1,36 @@
+const cityInput = document.getElementById("input");
+
+const submitForm = cityInput.addEventListener("keypress", (e) => {
+    if (e.key === 'Enter'){
+        let searchValue = cityInput.value.toLowerCase();
+        fetchCurrentWeather(searchValue)
+            .then ((data) => {renderCurrentData(data)});
+    }
+});
+
+const fetchCurrentWeather = async function (location) {
+    const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=' + location + '&units=imperial&appid=02cc3663df23cce74bc99a14efa05618');
+    return await response.json();
+}
+
+const renderCurrentData = (data) => {
+    currentTemp.innerHTML = Math.round(data.main.temp) + '&deg;F'
+}
+
+const currentTemp = document.getElementById("currentTemp");
+
+
+
+
+
+
+
+
+
+async function fetchDummy() {
+    const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=tyler&units=imperial&appid=02cc3663df23cce74bc99a14efa05618');
+    const data = await response.json();
+    console.log(data);
+}
+
+fetchDummy();
