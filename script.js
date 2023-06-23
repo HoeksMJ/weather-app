aedf412924935e17d17f053aa9a685a595f4ebb4
HoeksMJ <hoeksmj@gmail.com>
2023-06-22 21:41:00 -0500

    add fetchFutureWeather, add renders to renderCurrentData

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const submitForm = cityInput.addEventListener("keypress", (e) => {
         let searchValue = cityInput.value.toLowerCase();
         fetchCurrentWeather(searchValue)
             .then ((data) => {renderCurrentData(data)});
+        fetchFutureWeather(searchValue)
+            .then ((data) => {renderFutureData(data)});
         cityInput.blur();
     }
 });
@@ -14,14 +16,23 @@ const fetchCurrentWeather = async function (location) {
     return await response.json();
 }
 
+const fetchFutureWeather = async function (location) {
+    const response = await fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + location + '&units=imperial&appid=02cc3663df23cce74bc99a14efa05618');
+    return await response.json();
+}
+
 const renderCurrentData = (data) => {
     cityInput.value = data.name;
     currentTemp.innerHTML = Math.round(data.main.temp) + '&deg;F';
     cloudCondition.innerHTML = data.weather[0].description;
     currentLow.innerHTML = Math.round(data.main.temp_min) + '&deg;F';
     currentHigh.innerHTML = Math.round(data.main.temp_max) + '&deg;F';
-    currentRainChance.innerHTML = 'Chance of rain: '
+    currentWindSpeed.innerHTML = Math.round(10*data.wind.speed)/10 + ' mph';
 
+}
+
+const renderFutureData = (data) => {
+    
 }
 
 const cityText = document.getElementById("input");
@@ -29,7 +40,7 @@ const currentTemp = document.getElementById("currentTemp");
 const cloudCondition = document.getElementById("weatherText");
 const currentLow = document.getElementById("currentLow");
 const currentHigh = document.getElementById("currentHigh");
-const currentRainChance = document.getElementById("rainChance");
+const currentWindSpeed = document.getElementById("windSpeed");
 
 
 
@@ -41,9 +52,18 @@ const currentRainChance = document.getElementById("rainChance");
 
 
 async function fetchDummy() {
-    const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=tyler&units=imperial&appid=02cc3663df23cce74bc99a14efa05618');
+    const response = await fetch('https://api.openweathermap.org/data/2.5/forecast?q=tyler&units=imperial&appid=02cc3663df23cce74bc99a14efa05618');
     const data = await response.json();
     console.log(data);
 }
 
 fetchDummy();
+
+async function fetchDummyTwo() {
+    const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=tyler&units=imperial&appid=02cc3663df23cce74bc99a14efa05618');
+    const data = await response.json();
+    console.log(data);
+}
+
+fetchDummyTwo();
+
